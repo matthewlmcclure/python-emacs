83b3dfbb0593d37021624a8971e7d0549ab6d8f9
Matt McClure <matt.mcclure@mapmyfitness.com>
2012-07-20 17:29:52 -0400

    Files from Emacs 24.1.50.1 2012-06-10. Plus a setup.py and .gitignore.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='emacs',
+    version='24.1.50.1-20120610',
+    packages=[],
+    py_modules=['emacs', 'emacs2', 'emacs3'],
+    license='GPL 3',
+)
