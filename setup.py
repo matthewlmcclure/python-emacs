08a8f713f14148af3e6dc894c213fcad2869f107
Matt McClure <matt.mcclure@mapmyfitness.com>
2012-07-20 17:35:03 -0400

    Enough metadata to submit to PyPI.

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from distutils.core import setup
 setup(
     name='emacs',
     version='24.1.50.1-20120610',
+    author='Dave Love',
+    author_email='fx@gnu.org',
+    url='https://github.com/matthewlmcclure/python-emacs',
     packages=[],
     py_modules=['emacs', 'emacs2', 'emacs3'],
     license='GPL 3',
